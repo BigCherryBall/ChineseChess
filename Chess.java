e1a922fcc4ca9202aa9315d87840d2269a7eb3b7
BigCherryBall <2655602003@qq.com>
2023-11-02 23:01:46 +0800

    update

--- Chess.java
+++ Chess.java
@@ -5,11 +5,11 @@ public abstract class Chess {
     public Team team;
     public Vector2 pos;
     public Vector2 init_pos;
-    public BufferedImage image;
+    public BufferedImage img;
 
     public abstract MoveInfo move(String command, Chess[][] map);
 
-    public abstract MoveInfo isLegal(String command);
+    public abstract MoveInfo isLegal(String command, Chess[][] map);
 
     /*
      * 每次移动记得更新位置
@@ -29,6 +29,80 @@ public abstract class Chess {
         this.pos.y = this.init_pos.y;
     }
 
+    public int getCowByNum(String num) {
+        switch (num) {
+            case "1", "一" -> {
+                if (this.team == Team.red) {
+                    return 8;
+                }
+                else {
+                    return 0;
+                }
+            }
+            case "2", "二" -> {
+                if (this.team == Team.red) {
+                    return 7;
+                }
+                else {
+                    return 1;
+                }
+            }
+            case "3", "三" -> {
+                if (this.team == Team.red) {
+                    return 6;
+                }
+                else {
+                    return 2;
+                }
+            }
+            case "4", "四" -> {
+                if (this.team == Team.red) {
+                    return 5;
+                }
+                else {
+                    return 3;
+                }
+            }
+            case "5", "五" -> {
+                return 4;
+            }
+            case "6", "六" -> {
+                if (this.team == Team.red) {
+                    return 3;
+                }
+                else {
+                    return 5;
+                }
+            }
+            case "7", "七" -> {
+                if (this.team == Team.red) {
+                    return 2;
+                }
+                else {
+                    return 6;
+                }
+            }
+            case "8", "八" -> {
+                if (this.team == Team.red) {
+                    return 1;
+                }
+                else {
+                    return 7;
+                }
+            }
+            case "9", "九" -> {
+                if (this.team == Team.red) {
+                    return 0;
+                }
+                else {
+                    return 8;
+                }
+            }
+        }
+
+        return -1;
+    }
+
     @Override
     public String toString() {
         return this.team.toString() + ":" + this.name;
@@ -40,7 +114,22 @@ public abstract class Chess {
         this.team = team;
         this.init_pos = init_pos;
         this.pos = new Vector2(init_pos.x, init_pos.y);
-        this.image = image;
+        this.img = image;
+    }
+
+    public static int getDisByNum(String num) {
+        return switch (num) {
+            case "1", "一" -> 1;
+            case "2", "二" -> 2;
+            case "3", "三" -> 3;
+            case "4", "四" -> 4;
+            case "5", "五" -> 5;
+            case "6", "六" -> 6;
+            case "7", "七" -> 7;
+            case "8", "八" -> 8;
+            case "9", "九" -> 9;
+            default ->       -1;
+        };
     }
 
 }
--- Chesses.java
+++ Chesses.java
@@ -1,24 +1,48 @@
+import java.awt.image.BufferedImage;
 import java.io.File;
 
 class Horse extends Chess {
-
-
     @Override
     public MoveInfo move(String command, Chess[][] map) {
-        throw new UnsupportedOperationException("Unimplemented method 'move'");
+        MoveInfo info = this.isLegal(command, map);
+        return info;
+
     }
 
     @Override
-    public MoveInfo isLegal(String command) {
-        throw new UnsupportedOperationException("Unimplemented method 'isLegal'");
+    public MoveInfo isLegal(String command, Chess[][] map)
+    {
+        MoveInfo info = new MoveInfo();
+        String dir = String.valueOf(command.charAt(0));
+        String where = String.valueOf(command.charAt(1));
+        int cow = getCowByNum(where);
+        int dy = cow - this.pos.y;
+
+        //马不可以平
+        if(dir.equals("平"))
+        {
+            return null;
+        }
+        //进
+        else if (dir.equals("进"))
+        {
+            //横着走
+            if(dy == 2)
+            {
+                if(this.pos.y > 6)
+                {
+                    return null;
+                }
+            }
+        }
+
+        return info;
     }
 
-    public Horse(Team team, int x, int y, String img)
-    {
-        this.team = team;
-        this.init_pos.x = x;
-        this.init_pos.y = y;
-        new File(".");
+
+
+    public Horse(String name, Team team, int x, int y, BufferedImage img) {
+        super(name, team, new Vector2(x, y), img);
     }
-    
+
 }
